31110c3a773aca60491f231b9696f536d00b47d9
Mehmet Yavuz Katrancı <77459135+myavkat@users.noreply.github.com>
2026-04-30 17:06:12 +0300

    update spring boot version to 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
 	java
 	war
-	id("org.springframework.boot") version "3.5.3"
-	id("io.spring.dependency-management") version "1.1.0"
+	id("org.springframework.boot") version "4.0.6"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.sbm"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_25
+	targetCompatibility = JavaVersion.VERSION_25
 }
 
 repositories {
@@ -22,20 +22,21 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	
-	implementation("org.webjars:bootstrap:5.3.0")
+	implementation("org.webjars:bootstrap:5.3.8")
 	implementation("org.webjars:popper.js:2.9.3")
-	implementation("org.webjars:webjars-locator:0.47")
+	implementation("org.webjars:webjars-locator-lite")
 	implementation("org.webjars:font-awesome:6.4.0")
 	
 	implementation("commons-validator:commons-validator:1.7")
 	implementation("commons-codec:commons-codec:1.7")
+	implementation("org.apache.commons:commons-lang3:3.20.0")
 	
 	runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
 	runtimeOnly("io.r2dbc:r2dbc-mssql")
 	
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("io.projectreactor:reactor-test")
 	
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
